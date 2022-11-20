20a9e8a9659857f5e188fcf4a88d2e69776d2784
kwing25 <kendracwing@icloud.com>
2022-11-19 18:55:45 -0700

    ➕☑️ Added ```global-styles.js``` for project style

---
+++ src/styles/global-styles.js
@@ -0,0 +1,19 @@
+
+const colors = {
+    primaryPink: '#ED2290',
+    neutralTransparent: 'transparent'
+}
+
+const globalStyles = {
+    body: {
+        fontFamily: "Botang",
+        backgroundColor: colors.primaryPink,
+    },
+
+    h1: {
+        fontSize: 130,
+
+    }
+};
+
+export default globalStyles;
