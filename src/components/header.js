464690ca9a159e2db889bd177c1fa98842a6ae14
kwing25 <kendracwing@icloud.com>
2022-11-19 18:58:22 -0700

    ➕🖊️ Created ```header``` component

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import { makeStyles } from "tss-react/mui";
+import Lottie from "lottie-react";
+import mapIconPulseAnimation from "../assets/lotties/mapIconPulse.json";
+
+function Header() {
+    const { classes } = useStyles({});
+    
+  return (
+    <div className="header">
+      <h1>Header</h1>
+      <div className={classes.wrapper}>
+        <Lottie animationData={mapIconPulseAnimation} />
+      </div>
+    </div>
+  );
+}
+
+const useStyles = makeStyles()((_, props) => ({
+
+	wrapper: {
+		height: '50vh',
+        width: '50vw'
+	}
+}))
+
+
+export default Header;
