8f06bd3628d9d50cec36ae8b80ae7022b3a520d2
kwing25 <kendracwing@icloud.com>
2022-11-21 18:03:42 -0700

    🗑️📦 Removed service worker and workbox...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import {GlobalStyles} from 'tss-react'
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import reportWebVitals from './reportWebVitals';
 import "./styles/fonts.css"
 import globalStyles from './styles/global-styles'
@@ -24,10 +24,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
