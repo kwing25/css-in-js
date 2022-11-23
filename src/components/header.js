64c87d9d6061de22f03818f0bf306a0701f2bdad
kwing25 <kendracwing@icloud.com>
2022-11-22 21:53:48 -0700

    ➕📝 Added and edited various files

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,25 @@
 import { makeStyles } from "tss-react/mui";
 import Lottie from "lottie-react";
 import mapIconPulseAnimation from "../assets/lotties/mapIconPulse.json";
+import rocketSpaceAnimation from "../assets/lotties/rocketSpace.json";
 
 function Header() {
   const { classes } = useStyles({});
 
   return (
     <div className="header">
-      <h1>Header</h1>
+      <h1>Planet Express</h1>
+      <h5>A delivery service that is out of this world!</h5>
       <div className={classes.wrapper}>
-        <Lottie animationData={mapIconPulseAnimation} />
+        <Lottie animationData={rocketSpaceAnimation} />
+
+
+        {/* We deliver to the stars and beyond.
+          We are the fastest delivery service in the universe.
+          Our crew is replaceable, your package isn't!  */}
+
       </div>
-      <p>sub paragrah</p>
+
     </div>
   );
 }
@@ -20,7 +28,8 @@ const useStyles = makeStyles()((_, props) => ({
 
   wrapper: {
     height: '50vh',
-    width: '50vw'
+    width: '50vw',
+    textAlign: 'center',
   }
 }))
 
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,19 +1,37 @@
 
 const colors = {
-    primaryPink: '#ED2290',
+    brandTeal: '#00D09F',
+    brandRed: '#D83A3A',
+    brandWhite: '#F8F2D6',
+    brandBlack: '#010100',
+    secondaryBlue: '#408975',
+    secondarkTeal: '#A8F9B0',
+    secondaryDark: '#8F3532',
+    neutralBlack: '#000000',
+    neutralWhite: '#ffffff',
     neutralTransparent: 'transparent'
 }
 
 const globalStyles = {
     body: {
-        fontFamily: "Comfortaa",
-        backgroundColor: colors.primaryPink,
+fontFamily: "Outfit",
+        overscrollBehaviorY: "none",
+    
     },
 
     h1: {
-        fontFamily: "Botang",
+        fontFamily: "OutfitBold",
         fontSize: 130,
+        lineHeight: "1",
+        letterSpacing: "-1px",
+        color: colors.brandTeal,
 
+    },
+
+    p: {
+        fontFamily: "FRReg",
+        fontSize: 20,
+        letterSpacing: "1px",
     }
 };
 
